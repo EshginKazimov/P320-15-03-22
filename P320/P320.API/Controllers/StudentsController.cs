230dcd1fd94f9fb874f5e48acf5751aa70a67f37
EshginKazimov <82080797+EshginKazimov@users.noreply.github.com>
2022-03-15 23:57:33 +0400

    Add files via upload

---
+++ P320/P320.API/Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P320.DomainModels.DTOs;
+using P320.DomainModels.Entities;
+using P320.Repository.DataContext;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace P320.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public StudentsController(AppDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var students = await _dbContext.Students.ToListAsync();
+
+            return Ok(_mapper.Map<List<StudentDto>>(students));
+        }
+
+        [HttpGet("{id?}")]
+        public async Task<IActionResult> Get([FromRoute] int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _dbContext.Students.FindAsync(id);
+            if (student == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<StudentDto>(student));
+        }
+    }
+}
---
+++ P320/P320.DomainModels/DTOs/StudentDto.cs
@@ -0,0 +1,15 @@
+﻿using P320.DomainModels.Base;
+
+namespace P320.DomainModels.DTOs
+{
+    public class StudentDto : IDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+    }
+}
